3bd7be5bd3d104136f8e98a119b572ce02b2d5e9
EaseWay Hu <easeway@gmail.com>
2013-12-13 15:19:38 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Partitioner: require('./lib/Partitioner'),
+    ClusterPartitioner: require('./lib/ClusterPartitioner'),
+    PartitionMap:    require('./lib/PartitionMap'),
+    PartitionMapper: require('./lib/PartitionMapper'),
+    ConnectorClient: require('./lib/ConnectorClient'),
+    ConnectorStates: require('./lib/ConnectorStates')
+};
